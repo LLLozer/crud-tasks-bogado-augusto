7e5b4e548f7a45c1d09377fb8c6c6e5c68057399
LLLozer <imtheshittyuser@outlook.com>
2025-08-06 14:06:57 -0300

    Modelo task-model.js configurado

---
+++ src/models/task-model.js
@@ -0,0 +1,11 @@
+import { DataTypes } from "sequelize"
+import {sequelize} from "../config/database.js"
+
+const Task = sequelize.define("Task",{
+    id: { primaryKey: true, type:DataTypes.INTEGER, allowNull: false, autoIncrement: true },
+    title: { type: DataTypes.STRING(100), allowNull: false },
+    description: { type: DataTypes.STRING(100), allowNull: false },
+    isComplete: { type: DataTypes.BOOLEAN, defaultValue: false}
+})
+
+export default Task
