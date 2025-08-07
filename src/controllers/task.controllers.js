bbd07ae557d75c1563c65be6579f5697c00ae3a0
LLLozer <imtheshittyuser@outlook.com>
2025-08-07 10:50:56 -0300

    Controlador para actualizar creado con algunas validaciones, faltan validaciones para campos title y description

--- src/controllers/task.controllers.js
+++ src/controllers/task.controllers.js
@@ -21,3 +21,46 @@ const findTaskById = async (req , res) => {
     const findID = await Task.findByPk(taskID)
     res.status(200).json(findID)
 }
+
+const updateTask = async (req , res) => {
+    const taskID = parseInt(req.params.id)
+    const { title, description, isComplete } = req.body
+    if (isNaN(taskID)) {
+        res.status(400).json ({
+            message:"Error: El ID debe ser un número.",
+            error:"Bad Request",
+            statusCode: "400"
+        })
+    }
+    if (!title && !description) {
+        res.status(400).json ({
+            message: "Error: Algunos campos están vacíos.",
+            error: "Bad request",
+            statusCode: "400"
+        })
+    }
+    try {
+        const findID = await Task.findByPk(taskID)
+        if (!findID) {
+            res.status(404).json ({
+                message: "Error: Ese ID no existe.",
+                error: "Bad request",
+                statusCode: "404"
+            })
+        }
+        await findID.update({ title, description, isComplete})
+        if (isComplete !== (true || false)) {
+            res.status(400).json ({
+                message: "Error: isComplete debe ser booleano.",
+                error: "Bad request",
+                statusCode: "400"
+            })
+        }
+    } catch (error) {
+        res.status(400).json ({
+            message: "Error: Error al actualizar los datos.",
+            error: "Bad request",
+            statusCode: "400"
+        })
+    }
+}
