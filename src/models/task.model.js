27a66ea1aa989d667a4131b15eceede4cf92bab4
LLLozer <imtheshittyuser@outlook.com>
2025-08-13 12:30:13 -0300

    Agregado timestamps en task.models

--- src/models/task.model.js
+++ src/models/task.model.js
@@ -1,11 +1,26 @@
-import { DataTypes } from "sequelize"
-import {sequelize} from "../config/database.js"
+import { DataTypes } from "sequelize";
+import { sequelize } from "../config/database.js";
 
-const Task = sequelize.define("Task",{
-    id: { primaryKey: true, type:DataTypes.INTEGER, allowNull: false, autoIncrement: true },
+const Task = sequelize.define(
+  "Task",
+  {
+    id: {
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+    },
     title: { type: DataTypes.STRING(100), allowNull: false },
     description: { type: DataTypes.STRING(100), allowNull: false },
-    isComplete: { type: DataTypes.BOOLEAN, defaultValue: false, allowNull: false}
-})
+    isComplete: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: false,
+  }
+);
 
-export default Task
+export default Task;
