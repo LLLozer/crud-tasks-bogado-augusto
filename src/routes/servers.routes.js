6c58d8e6134c66e5dec2b20c435b829c692335a7
LLLozer <imtheshittyuser@outlook.com>
2025-08-18 10:09:29 -0300

    Creado controlador para user_server

--- src/routes/servers.routes.js
+++ src/routes/servers.routes.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
 const serverRouter = Router()
-import { find_all_servers, create_server, find_server_by_id } from "../controllers/servers.controllers.js";
+import { findAllServers, createServer, findServerById } from "../controllers/servers.controllers.js";
 
-serverRouter.get("/servers", find_all_servers)
-serverRouter.get("/servers/:id", find_server_by_id)
-serverRouter.post("/servers/", create_server)
+serverRouter.get("/servers", findAllServers)
+serverRouter.get("/servers/:id", findServerById)
+serverRouter.post("/servers/", createServer)
 
 export default serverRouter
