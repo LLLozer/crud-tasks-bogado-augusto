aa443ff02144698e6ba375216e67d6daa6d0fafd
LLLozer <imtheshittyuser@outlook.com>
2025-08-24 19:07:08 -0300

    Validaciones para accountinfo creadas. Exportadas y agregadas a su respectiva ruta

---
+++ src/middlewares/account.info.validations.js
@@ -0,0 +1,104 @@
+import { body, param } from "express-validator";
+import { AccountInfo } from "../models/account.info.model.js";
+import { Op } from "sequelize";
+
+export const createAccValidation = [
+  body("first_name")
+    .notEmpty()
+    .isString()
+    .withMessage("El campo first_name debe ser obligatorio"),
+  body("last_name")
+    .notEmpty()
+    .isString()
+    .withMessage("El campo last_name debe ser obligatorio"),
+  body("dni")
+    .notEmpty()
+    .withMessage("El campo dni debe ser obligatorio")
+    .isInt({ min: 1 })
+    .withMessage("El campo DNI debe ser un numero entero positivo")
+    .custom((value) => {
+      if (value.toString().length < 8) {
+        throw new Error("El campo DNI debe tener al menos 8 digitos");
+      }
+      return true;
+    })
+    .custom(async (value) => {
+      const accountExists = await AccountInfo.findOne({
+        where: { dni: value },
+      });
+      if (accountExists) {
+        throw new Error("Ese DNI ya existe");
+      }
+      return true;
+    }),
+];
+
+export const updateAccValidation = [
+  param("id")
+    .isInt()
+    .withMessage("El id debe ser un entero")
+    .custom(async (value) => {
+      const account = await AccountInfo.findByPk(value);
+      if (!account) {
+        throw new Error("La cuenta no existe");
+      }
+    }),
+  body("first_name")
+    .optional()
+    .isString()
+    .withMessage("El campo name debe ser una cadena de caracteres")
+    .isLength({ min: 2, max: 10 })
+    .withMessage("El first_name debe ser entre 2 y 10 caracteres"),
+  body("last_name")
+    .optional()
+    .notEmpty()
+    .isLength({ min: 2, max: 10 })
+    .withMessage(
+      "El campo last_name debe ser obligatorio y de 2 a 10 caracteres de longitud"
+    ),
+  body("dni")
+    .optional()
+    .isInt({ min: 1 })
+    .withMessage("El campo DNI debe ser un numero entero positivo")
+    .custom((value) => {
+      if (value.toString().length < 8) {
+        throw new Error("El campo DNI debe tener al menos 8 digitos");
+      }
+      return true;
+    })
+    .custom(async (value) => {
+      const accountExists = await AccountInfo.findOne({
+        where: { dni: value, id: { [Op.ne]: req.params.id } },
+      });
+      if (accountExists) {
+        throw new Error("Ese DNI ya existe");
+      }
+      return true;
+    }),
+];
+
+export const getAccIDValidation = [
+  param("id")
+    .exists()
+    .isInt({ min: 1 })
+    .withMessage("El campo ID debe ser un numero")
+    .custom(async (value) => {
+      const account = await AccountInfo.findByPk(value);
+      if (!account) {
+        throw new Error("La cuenta no existe");
+      }
+    }),
+];
+
+export const deleteAccValidation = [
+  param("id")
+    .exists()
+    .isInt({ min: 1 })
+    .withMessage("El campo ID debe ser un numero")
+    .custom(async (value) => {
+      const account = await AccountInfo.findByPk(value);
+      if (!account) {
+        throw new Error("La cuenta no existe");
+      }
+    }),
+];
--- src/routes/account.info.routes.js
+++ src/routes/account.info.routes.js
@@ -2,10 +2,14 @@ import { Router } from "express";
 const routerAccountInfo = Router()
 import { createAccount, findAllAccounts, findAccById, updateAccount, deleteAcc } from "../controllers/account.info.controllers.js";
 
-routerAccountInfo.post("/account_info", createAccount);
+import { createAccValidation, updateAccValidation, getAccIDValidation, deleteAccValidation } from "../middlewares/account.info.validations.js"
+
+import { validationResult } from "../middlewares/validator/validator.js"
+
+routerAccountInfo.post("/account_info", createAccValidation, validationResult, createAccount);
 routerAccountInfo.get("/account_info", findAllAccounts);
-routerAccountInfo.get("/account_info/:id", findAccById);
-routerAccountInfo.put("/account_info/:id", updateAccount);
-routerAccountInfo.delete("/account_info/:id", deleteAcc);
+routerAccountInfo.get("/account_info/:id", getAccIDValidation, validationResult, findAccById);
+routerAccountInfo.put("/account_info/:id", updateAccValidation, validationResult, updateAccount);
+routerAccountInfo.delete("/account_info/:id", deleteAccValidation, validationResult, deleteAcc);
 
 export default routerAccountInfo
